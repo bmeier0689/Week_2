e88cfae6f4e7d19459ae22ef515e54d510a9a76a
Brad Meier <bmeier0689@gmail.com>
2022-04-16 16:19:21 +0900

    Saturday Changes

---
+++ script.js
@@ -0,0 +1,18 @@
+var stringVariable = "Web Fundamentals"
+var charactervariable = "W"
+var numberVariable = 58
+var stringNumber = "58"
+numberVariable = numberVariable + 2 // 60
+stringNumber = stringNumber + 2 // "582"
+console.log(numberVariable)
+console.log(stringNumber)
+var booleanVariable = true
+var booleantwo = false
+var arrayVariable = ["Nuggets", 43, numberVariable, false, [1,2]]
+//                      0       1       2              3     4
+console.log(arrayVariable[4][1])
+var objectVariable = {
+    'name':"Clinks",
+    "class":"Webfund"
+}
+console.log(objectVariable['name'])
